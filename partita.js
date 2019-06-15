1a5b6ea5daa6039aae0d9664afd12b7c1df0230d
Lachlan Marnham <lachlan.marnham@farfetch.com>
2019-06-15 18:05:02 +0100

    Pull in js and css, and hot load jquery if necessary

--- partita.js
+++ partita.js
@@ -75,5 +75,33 @@ function responsiveSize() {
 }
 
 
-$(window).resize(responsiveSize);
-$(document).ready(responsiveSize);
+function importWithScript(scriptType, scriptPath) {
+    var script = document.createElement('script');
+    script.type = scriptType;
+    script.src = scriptPath;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+
+function importJqueryIfAbsent() {
+    if(!window.jQuery) {
+        importWithScript("text/javascript", "https://code.jquery.com/jquery-3.3.1.min.js");
+    }
+}
+
+
+function sleep(seconds) {
+    milliseconds = seconds * 1000;
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+
+async function runPartita() {
+    importJqueryIfAbsent();
+    await sleep(3);
+    $(window).resize(responsiveSize);
+    $(document).ready(responsiveSize);
+    console.log('done');
+}
+
+runPartita();
