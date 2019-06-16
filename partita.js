134f8e2ea51bf1d39a32ef3358642b512c35e608
Lachlan Marnham <lachlan.marnham@farfetch.com>
2019-06-16 14:26:29 +0100

    Move import helpers to utils

--- partita.js
+++ partita.js
@@ -77,29 +77,9 @@ function responsiveSize() {
 }
 
 
-function importWithScript(scriptType, scriptPath) {
-    var script = document.createElement('script');
-    script.type = scriptType;
-    script.src = scriptPath;
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-
-async function importJqueryIfAbsent() {
-    if(!window.jQuery) {
-        importWithScript("text/javascript", "https://code.jquery.com/jquery-3.3.1.min.js");
-
-        // Don't return control until jQuery is usable
-        while(!window.jQuery) {
-            await utils.sleep(50);
-        }
-    }
-}
-
-
 async function runPartita() {
     // Ensure external dependencies are present. If not, load them before carrying on.
-    await importJqueryIfAbsent();
+    await utils.importJqueryIfAbsent();
     $(window).resize(responsiveSize);
     $(document).ready(responsiveSize);
 }
--- utils.js
+++ utils.js
@@ -2,4 +2,24 @@ function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-export {sleep};
+
+function importWithScript(scriptType, scriptPath) {
+    var script = document.createElement('script');
+    script.type = scriptType;
+    script.src = scriptPath;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+
+async function importJqueryIfAbsent() {
+    if(!window.jQuery) {
+        importWithScript("text/javascript", "https://code.jquery.com/jquery-3.3.1.min.js");
+
+        // Don't return control until jQuery is usable
+        while(!window.jQuery) {
+            await sleep(50);
+        }
+    }
+}
+
+export {importJqueryIfAbsent};
