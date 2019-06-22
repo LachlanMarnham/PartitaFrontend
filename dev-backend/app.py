63ab2a903c58fd3853317e7ae5a46f5e9cd3c1ab
Lachlan Marnham <lachlan.marnham@farfetch.com>
2019-06-22 17:38:29 +0100

    Make dev server serve list of scales

--- dev-backend/app.py
+++ dev-backend/app.py
@@ -1,11 +1,22 @@
+import json
+
 from flask import Flask
+
+from entities.item_types import Scale
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'hello, world'
+    response = {}
+
+    a_minor = Scale(iid=1, title="A Minor", sort_index=0)
+    b_major = Scale(iid=2, title="B Major", sort_index=1)
+    c_diminished = Scale(iid=3, title="C Diminished", sort_index=2)
+
+    response['scales'] = [a_minor.to_dict(), b_major.to_dict(), c_diminished.to_dict()]
+    return json.dumps(response), 200
 
 
 app.run(host='0.0.0.0', port=5678, debug=True)
